5c823f3d5f6c047aa27cfa24851c6b94060884f5
mkharshita <mkharshita5@gmail.com>
2024-09-28 22:45:25 +0530

    image uploading to dynamic path

--- src/main/java/com/api/book/bootrestbook/controllers/FileUploadController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/FileUploadController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.api.book.bootrestbook.helper.FileUploadHelper;
 
@@ -18,6 +19,7 @@ public class FileUploadController {
     @Autowired
     private FileUploadHelper fileUploadHelper;
 
+    @SuppressWarnings("null")
     @PostMapping("/upload-file")    
     public ResponseEntity<String> uplodFile(@RequestParam("file") MultipartFile file){
         // System.out.println(file.getOriginalFilename());
@@ -35,12 +37,19 @@ public class FileUploadController {
             }
 
             boolean status = this.fileUploadHelper.uploadFile(file);
-            return ResponseEntity.ok("Upload Successfully");
+            if(status){
+                return ResponseEntity.ok(
+                    ServletUriComponentsBuilder.fromCurrentContextPath().
+                    path("/image/").
+                    path(file.getOriginalFilename()).
+                        toUriString()
+                    );
+            }
 
         } catch (Exception e){
             e.printStackTrace();
         }
-        
+
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong! Try Again!");
     }
 }
--- src/main/java/com/api/book/bootrestbook/helper/FileUploadHelper.java
+++ src/main/java/com/api/book/bootrestbook/helper/FileUploadHelper.java
@@ -1,19 +1,27 @@
 package com.api.book.bootrestbook.helper;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
     
-    public final String UPLOAD_DIR = "/Users/harshitasaxena/Documents/Sts/bootrestbook/src/main/resources/static/image";
+    //public final String UPLOAD_DIR = "/Users/harshitasaxena/Documents/Sts/bootrestbook/src/main/resources/static/image";
 
+    public final String UPLOAD_DIR = new ClassPathResource("static/image/").getFile().getAbsolutePath();
+
+    public FileUploadHelper() throws IOException{
+
+    }
+    
     public boolean uploadFile(MultipartFile file){
         boolean status = false;
         try {
